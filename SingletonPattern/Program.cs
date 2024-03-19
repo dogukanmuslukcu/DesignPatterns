bb38fdeb25002c9a7cdbdef14170b2b4c7994269
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-03-19 14:26:41 +0300

    Singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using SingletonPattern;
+// BEFORE SINGLETON
+
+/* 
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+
+var countryProvider = new CountryProvider();
+var countries = await countryProvider.GetCountries();
+foreach (var country in countries) 
+{
+    Console.WriteLine(country.Name);
+}
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+var countries2 = await countryProvider.GetCountries();
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+*/
+
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+
+var countries = await CountryProvider.Instance.GetCountries();
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+
+var countries2 = await CountryProvider.Instance.GetCountries();
+foreach (var country in countries2)
+{
+    Console.WriteLine(country.Name);
+}
+Console.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
